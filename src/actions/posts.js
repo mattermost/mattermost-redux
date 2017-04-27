f6551353e2db78b8d06bb43df1848f7b005499fd
Harrison Healey <harrisonmhealey@gmail.com>
2017-04-27 09:05:37 -0400

    Changed Object.keys to Object.values

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -305,8 +305,7 @@ async function getProfilesAndStatusesForPosts(list, dispatch, getState) {
     const userIdsToLoad = new Set();
     const statusesToLoad = new Set();
 
-    Object.keys(posts).forEach((postId) => {
-        const post = posts[postId];
+    Object.values(posts).forEach((post) => {
         const userId = post.user_id;
 
         if (userId === currentUserId) {
@@ -346,9 +345,7 @@ export function getNeededAtMentionedUsernames(state, posts) {
 
     const usernamesToLoad = new Set();
 
-    Object.keys(posts).forEach((postId) => {
-        const post = posts[postId];
-
+    Object.values(posts).forEach((post) => {
         if (!post.message.includes('@')) {
             return;
         }
