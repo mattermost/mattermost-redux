a5bd6d8e91364dcdd458358372a1236ae0cf53a3
JoramWilander <jwawilander@gmail.com>
2017-05-02 20:43:20 -0400

    Add map selector for emojis

--- src/selectors/entities/emojis.js
+++ src/selectors/entities/emojis.js
@@ -1,7 +1,19 @@
 // Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
 // See License.txt for license information.
 
+import {createSelector} from 'reselect';
+
 export function getCustomEmojis(state) {
     return state.entities.emojis.customEmoji;
 }
 
+export const getCustomEmojisAsMap = createSelector(
+    getCustomEmojis,
+    (emojis) => {
+        const map = new Map();
+        Object.keys(emojis).forEach((key) => {
+            map.set(key, emojis[key]);
+        });
+        return map;
+    }
+);
