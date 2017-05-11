43a596c4ed128f444aaddc5c2dc0624c2febb2e1
JoramWilander <jwawilander@gmail.com>
2017-05-11 09:37:37 -0400

    Add preference selectors for getting a single preference

--- src/selectors/entities/preferences.js
+++ src/selectors/entities/preferences.js
@@ -4,3 +4,24 @@
 export function getMyPreferences(state) {
     return state.entities.preferences.myPreferences;
 }
+
+function getKey(category, name) {
+    return `${category}--${name}`;
+}
+
+export function get(state, category, name, defaultValue = '') {
+    const key = getKey(category, name);
+    const prefs = getMyPreferences(state);
+
+    if (!(key in prefs)) {
+        return defaultValue;
+    }
+
+    return prefs[key].value;
+}
+
+export function getBool(state, category, name, defaultValue = false) {
+    const value = get(state, category, name, String(defaultValue));
+
+    return value !== 'false';
+}
