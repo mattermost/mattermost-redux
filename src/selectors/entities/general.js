5b676e2737c526617cd9b4e9c7dbb16f3615e269
Harrison Healey <harrisonmhealey@gmail.com>
2017-07-28 16:19:18 -0400

    PLT-6924 Added selectors for mobile file uploads/downloads (#213)

--- src/selectors/entities/general.js
+++ src/selectors/entities/general.js
@@ -1,6 +1,8 @@
 // Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
 // See License.txt for license information.
 
+import {createSelector} from 'reselect';
+
 export function getConfig(state) {
     return state.entities.general.config;
 }
@@ -12,3 +14,19 @@ export function getLicense(state) {
 export function getCurrentUrl(state) {
     return state.entities.general.credentials.url;
 }
+
+export const canUploadFilesOnMobile = createSelector(
+    getConfig,
+    (config) => {
+        // Defaults to true if either setting doesn't exist
+        return config.EnableMobileFileUpload !== 'false' && config.EnableFileAttachments !== 'false';
+    }
+);
+
+export const canDownloadFilesOnMobile = createSelector(
+    getConfig,
+    (config) => {
+        // Defaults to true if either setting doesn't exist
+        return config.EnableMobileFileDownload !== 'false' && config.EnableFileAttachments !== 'false';
+    }
+);
