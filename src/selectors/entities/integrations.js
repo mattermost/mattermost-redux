cdaf8f500d6383794fade7dcfbdcd319b350c0f3
kevin <kevin@loyal-info.com>
2017-08-15 20:43:57 +0800

    [IMP]add new selector to integrations (#225)

--- src/selectors/entities/integrations.js
+++ src/selectors/entities/integrations.js
@@ -1,6 +1,9 @@
 // Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
 // See License.txt for license information.
 
+import {createSelector} from 'reselect';
+import {getCurrentTeamId} from './teams';
+
 export function getIncomingHooks(state) {
     return state.entities.integrations.incomingHooks;
 }
@@ -16,3 +19,14 @@ export function getCommands(state) {
 export function getOAuthApps(state) {
     return state.entities.integrations.oauthApps;
 }
+
+/**
+ * get outgoing hooks in current team
+ */
+export const getOutgoingHooksInCurrentTeam = createSelector(
+    getCurrentTeamId,
+    getOutgoingHooks,
+    (teamId, hooks) => {
+        return Object.values(hooks).filter((o) => o.teamId === teamId);
+    }
+);
