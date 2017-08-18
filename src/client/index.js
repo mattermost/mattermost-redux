b65adc14d4c8cafb4259196d304b87b99ba88e23
Brett McBride <brett@deakin.edu.au>
2017-08-18 15:36:25 +1000

    Revert "add websocket to index.js"
    This reverts commit 23c9ff60b5ed1cd2dd9561b32a6709900d3e22b2.

--- src/client/index.js
+++ src/client/index.js
@@ -3,13 +3,11 @@
 
 import ClientClass from './client.js';
 import ClientClass4 from './client4.js';
-import WebSocketClient from './websocket_client.js';
 
 const Client = new ClientClass();
 const Client4 = new ClientClass4();
 
 export {
     Client,
-    Client4,
-    WebSocketClient
+    Client4
 };
