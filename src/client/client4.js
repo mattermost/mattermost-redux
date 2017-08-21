2ba1306a69e46f84a7143c2ce2753a1f222cb118
Brett McBride <brett@deakin.edu.au>
2017-08-21 12:45:08 +1000

    only set fetch's credentials:include option if no access token
    this is what client.js does, and fixes a CORS issue when running client4.js in a browser.
    Specifically, the issue seen is:
    
    Fetch API cannot load http://127.0.0.1:8065/api/v4/users/me. Response to preflight request doesn't pass access control
    check: The value of the 'Access-Control-Allow-Credentials' header in the response is '' which must be 'true' when the
    request's credentials mode is 'include'. Origin 'http://127.0.0.1:8002' is therefore not allowed access.

--- src/client/client4.js
+++ src/client/client4.js
@@ -211,9 +211,9 @@ export default class Client4 {
 
         if (this.token) {
             headers[HEADER_AUTH] = `${HEADER_BEARER} ${this.token}`;
+        } else {
+            newOptions.credentials = 'include';
         }
-
-        newOptions.credentials = 'include';
 
         if (this.userAgent) {
             headers[HEADER_USER_AGENT] = this.userAgent;
