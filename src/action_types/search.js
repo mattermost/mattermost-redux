15064beea81cecdcfead657ec4f334d1a464cb84
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2017-10-14 17:23:50 +0500

    Add search reducers

--- src/action_types/search.js
+++ src/action_types/search.js
@@ -8,6 +8,10 @@ export default keyMirror({
     SEARCH_POSTS_SUCCESS: null,
     SEARCH_POSTS_FAILURE: null,
 
+    /*
+     * The same action as `Constants.ActionTypes.RECEIVE_SEARCH`
+     */
+    RECEIVED_SEARCH: null,
     RECEIVED_SEARCH_POSTS: null,
     RECEIVED_SEARCH_TERM: null,
     REMOVE_SEARCH_POSTS: null,
--- src/reducers/entities/search.js
+++ src/reducers/entities/search.js
@@ -2,7 +2,7 @@
 // See License.txt for license information.
 
 import {combineReducers} from 'redux';
-import {SearchTypes, UserTypes} from 'action_types';
+import {SearchTypes, UserTypes, PostTypes} from 'action_types';
 
 function results(state = [], action) {
     switch (action.type) {
@@ -61,6 +61,113 @@ function recent(state = {}, action) {
     }
 }
 
+/*
+ * Search reducer for mattermost-webapp will replace `SearchStore`
+ * Structure is:
+ * {
+ *     term: string on null - searching terms,
+ *     searchResults: array or null - list of searched posts
+ *     isMentionSearch: boolean - is searched be mentions
+ *     isFlaggedPosts: boolean - is searched flagged posts
+ *     isPinnedPosts: boolean - is searched boolean posts
+ * }
+ */
+function search(
+    state = {
+        term: '',
+        searchResults: false,
+        isMentionSearch: false,
+        isFlaggedPosts: false,
+        isPinnedPosts: false
+    },
+    action
+) {
+    const {type, data} = action;
+
+    switch (type) {
+    case SearchTypes.RECEIVED_SEARCH: {
+        const {
+            results: searchResults,
+            is_mention_search: isMentionSearch,
+            is_flagged_posts: isFlaggedPosts,
+            is_pinned_posts: isPinnedPosts
+        } = data;
+        return {
+            ...state,
+            searchResults,
+            isMentionSearch,
+            isFlaggedPosts,
+            isPinnedPosts
+        };
+    }
+    case SearchTypes.RECEIVED_SEARCH_TERM: {
+        let updatedState = state;
+        const {do_search: doSearch, term} = data;
+        if (doSearch) {
+            updatedState = {
+                ...state,
+                searchResults: null,
+                isMentionSearch: false,
+                isFlaggedPosts: false,
+                isPinnedPosts: false,
+            };
+        }
+        updatedState = {...updatedState, term};
+        return updatedState;
+    }
+    case PostTypes.POST_DELETED:
+    case PostTypes.REMOVE_POST: {
+        const {searchResults} = state;
+
+        if (!searchResults) {
+            return state;
+        }
+
+        const {posts, order} = searchResults;
+
+        if (posts[data.id]) {
+            const updatedPosts = {...posts};
+            Reflect.deleteProperty(updatedPosts, data.id);
+            const updatedOrder = order.filter((item) => item.id !== data.id);
+            return {
+                ...state,
+                searchResults: {
+                    posts: updatedPosts,
+                    order: updatedOrder,
+                }
+            };
+        }
+        return state;
+    }
+    case PostTypes.RECEIVED_POST: {
+        const {searchResults} = state;
+
+        if (!searchResults) {
+            return state;
+        }
+
+        const {posts} = searchResults;
+        if (!posts[data.id]) {
+            return state;
+        }
+
+        const updatedPosts = {
+            ...posts,
+            [data.id]: data
+        };
+        return {
+            ...state,
+            searchResults: {
+                ...searchResults,
+                posts: updatedPosts
+            }
+        };
+    }
+    default:
+        return state;
+    }
+}
+
 export default combineReducers({
 
     // An ordered array with posts ids from the search results
@@ -68,5 +175,8 @@ export default combineReducers({
 
     // Object where every key is a team composed with
     // an object where the key is the term and the value indicates is "or" search
-    recent
+    recent,
+
+    //Reducer that will store information like StoreSearch in mattermost-webapp
+    search
 });
