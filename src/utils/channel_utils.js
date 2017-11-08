bff8819804ce79677c7e0e724d1c43fe93e5f592
Harrison Healey <harrisonmhealey@gmail.com>
2017-11-08 11:37:15 -0500

    Clean up channel ordering logic and export some helpers (#305)
    * Clean up channel ordering logic
    
    * Export isOpenChannel and isPrivateChannel helper functions
    
    * Fixed incorrectly named constants

--- src/utils/channel_utils.js
+++ src/utils/channel_utils.js
@@ -5,8 +5,12 @@ import {General, Preferences} from 'constants';
 import {displayUsername} from './user_utils';
 import {getPreferencesByCategory} from './preference_utils';
 
-const defaultPrefix = 'D'; // fallback for future types
-const typeToPrefixMap = {[General.OPEN_CHANNEL]: 'A', [General.PRIVATE_CHANNEL]: 'B', [General.DM_CHANNEL]: 'C', [General.GM_CHANNEL]: 'C'};
+const channelTypeOrder = {
+    [General.OPEN_CHANNEL]: 0,
+    [General.PRIVATE_CHANNEL]: 1,
+    [General.DM_CHANNEL]: 2,
+    [General.GM_CHANNEL]: 2
+};
 
 /**
  * Returns list of sorted channels grouped by type. Favorites here is considered as separated type.
@@ -427,17 +431,21 @@ function isNotDeletedChannel(channel) {
     return channel.delete_at === 0;
 }
 
-function isOpenChannel(channel) {
+export function isOpenChannel(channel) {
     return channel.type === General.OPEN_CHANNEL;
 }
 
-function isPrivateChannel(channel) {
+export function isPrivateChannel(channel) {
     return channel.type === General.PRIVATE_CHANNEL;
 }
 
 export function sortChannelsByTypeAndDisplayName(locale, a, b) {
-    if (a.type !== b.type && typeToPrefixMap[a.type] !== typeToPrefixMap[b.type]) {
-        return (typeToPrefixMap[a.type] || defaultPrefix).localeCompare((typeToPrefixMap[b.type] || defaultPrefix), locale);
+    if (a.type !== b.type) {
+        if (channelTypeOrder[a.type] < channelTypeOrder[b.type]) {
+            return -1;
+        }
+
+        return 1;
     }
 
     const aDisplayName = filterName(a.display_name);
