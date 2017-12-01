e30322825792ab6dace597c740ef0653d4ed7c48
Miguel Espinoza <espinozamiguel349@gmail.com>
2017-12-01 17:19:17 -0500

    Wait 10 seconds before failing

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -59,7 +59,7 @@ export function createPost(post, files = []) {
             meta: {
                 offline: {
                     canTimeout: true,
-                    timeout: 100,
+                    timeout: 10000,
                     effect: () => Client4.createPost({...newPost, create_at: 0}),
                     commit: (success, payload) => {
                         // Use RECEIVED_POSTS to clear pending posts
