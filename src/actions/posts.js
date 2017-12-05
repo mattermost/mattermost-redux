371f0e660c26dd348120db336d1e3659c924b344
Chris Duarte <csduarte@users.noreply.github.com>
2017-12-04 17:10:13 -0800

    Add canTimeout flag for mobile (#342)
    * Add timeout flags for mobile
    
    * Wait 10 seconds before failing

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -58,6 +58,8 @@ export function createPost(post, files = []) {
             },
             meta: {
                 offline: {
+                    canTimeout: true,
+                    timeout: 10000,
                     effect: () => Client4.createPost({...newPost, create_at: 0}),
                     commit: (success, payload) => {
                         // Use RECEIVED_POSTS to clear pending posts
