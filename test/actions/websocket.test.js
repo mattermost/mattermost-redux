f2a63d5c18c6c5d8d29b99778a24c8de1410bfe2
Elias Nahum <nahumhbl@gmail.com>
2017-12-06 19:10:07 -0300

    feedback review

--- test/actions/websocket.test.js
+++ test/actions/websocket.test.js
@@ -24,12 +24,11 @@ describe('Actions.Websocket', () => {
         store = await configureStore();
         await TestHelper.initBasic(Client4);
 
-        const connUrl = (Client4.getUrl() + '/api/v4/websocket');
-
+        const connUrl = (Client4.getUrl() + '/api/v4/websocket').replace(/^http:/, 'ws:');
         mockServer = new Server(connUrl);
         const webSocketConnector = TestHelper.isLiveServer() ? require('ws') : MockWebSocket;
         return await Actions.init(
-            'ios',
+            'web',
             null,
             null,
             webSocketConnector
