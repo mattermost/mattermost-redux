95f2763262e6235f534b4cb58071b0ed64f63ad5
Jesse Hallam <jesse.hallam@gmail.com>
2018-05-10 17:59:40 -0400

    add eslint-plugin-header, and --fix all

--- src/actions/general.js
+++ src/actions/general.js
@@ -1,5 +1,5 @@
-// Copyright (c) 2016-present Mattermost, Inc. All Rights Reserved.
-// See License.txt for license information.
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
 
 import {Client, Client4} from 'client';
 import {bindClientFunc, forceLogoutIfNecessary, FormattedError} from './helpers.js';
