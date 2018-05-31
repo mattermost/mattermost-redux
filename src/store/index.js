1daff84145119ddb448db8746a0afbaa5e4122fa
Jesús Espino <jespinog@gmail.com>
2018-06-01 00:10:15 +0200

    Add trivial flow migrations

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
+// @flow
 /* eslint-disable global-require, no-process-env */
 
 if (process.env.NODE_ENV === 'production') {
