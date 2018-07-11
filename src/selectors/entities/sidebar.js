7903228014b18c32a6331a9c86e876ae43742367
Miguel Espinoza <espinozamiguel349@gmail.com>
2018-07-11 08:40:28 -0400

    Changes from feedback

--- src/selectors/entities/sidebar.js
+++ src/selectors/entities/sidebar.js
@@ -1,6 +1,8 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import {createSelector} from 'reselect';
+
 import {General, Preferences} from 'constants/index';
 import {getConfig} from 'selectors/entities/general';
 import {getBool as getBoolPreference, get as getPreference} from 'selectors/entities/preferences';
@@ -12,32 +14,35 @@ const defaultSidebarPrefs = {
     sorting: 'alpha',
 };
 
-export function getSidebarPreferences(state) {
-    const config = getConfig(state);
-
-    const showUnreadSection = config.ExperimentalGroupUnreadChannels !== General.DISABLED && getBoolPreference(
-        state,
-        Preferences.CATEGORY_SIDEBAR_SETTINGS,
-        'show_unread_section',
-        config.ExperimentalGroupUnreadChannels === General.DEFAULT_ON
-    );
-
-    let sidebarPrefs = JSON.parse(
-        getPreference(
+export const getSidebarPreferences = createSelector(
+    (state) => {
+        const config = getConfig(state);
+        return config.ExperimentalGroupUnreadChannels !== General.DISABLED && getBoolPreference(
+            state,
+            Preferences.CATEGORY_SIDEBAR_SETTINGS,
+            'show_unread_section',
+            config.ExperimentalGroupUnreadChannels === General.DEFAULT_ON
+        );
+    },
+    (state) => {
+        const sidebarPreference = getPreference(
             state,
             Preferences.CATEGORY_SIDEBAR_SETTINGS,
             '',
             null
-        )
-    );
+        );
+        return JSON.parse(sidebarPreference);
+    },
+    (showUnreadSection, sidebarPreference) => {
+        let sidebarPrefs = sidebarPreference;
+        if (sidebarPrefs === null) {
+            // Support unread settings for old implementation
+            sidebarPrefs = {
+                ...defaultSidebarPrefs,
+                unreads_at_top: showUnreadSection ? 'true' : 'false',
+            };
+        }
 
-    if (sidebarPrefs === null) {
-        // Support unread settings for old implementation
-        sidebarPrefs = {
-            ...defaultSidebarPrefs,
-            unreads_at_top: showUnreadSection ? 'true' : 'false',
-        };
+        return sidebarPrefs;
     }
-
-    return sidebarPrefs;
-}
+);
