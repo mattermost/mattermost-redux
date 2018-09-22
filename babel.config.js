e765cbb8379ab94444dcaca25288c79a05bf7624
Hyeseong Kim <cometkim.kr@gmail.com>
2018-09-22 23:44:11 +0900

    Update Babel 7 configuration file
    - Change config file to babel.config.js
    - Add class-properties proposal
    - Enable loose mode

---
+++ babel.config.js
@@ -0,0 +1,28 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+module.exports = {
+    presets: [
+        ['@babel/preset-env', {
+            targets: {
+                ie: 11,
+                chrome: 43,
+                firefox: 52,
+                edge: 15,
+                ios: 9,
+            },
+            loose: true,
+            useBuiltIns: 'usage',
+            shippedProposals: true,
+        }],
+        '@babel/preset-flow',
+    ],
+    plugins: [
+        ['module-resolver', {
+            root: ['./src', '.'],
+        }],
+        ['@babel/proposal-class-properties', {
+            loose: true,
+        }],
+    ],
+};
