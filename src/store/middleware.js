86c1bb787abdb359dd6447b5eb2080bfc2bcc209
Harrison Healey <harrisonmhealey@gmail.com>
2018-10-05 09:54:25 -0400

    Move description of arguments back to configureStore and documented more fields

--- src/store/middleware.js
+++ src/store/middleware.js
@@ -11,13 +11,6 @@ const defaultOptions = {
     enableThunk: true,
 };
 
-/***
-clientOptions object - This param allows users to configure the store from the client side.
-It has the following properties:
-additionalMiddleware - func | array - Allows for single or multiple additional middleware functions to be passed in from the client side.
-enableBuffer - bool - default = true - If true, the store will buffer all actions until offline state rehydration occurs.
-enableThunk - bool - default = true - If true, include the thunk middleware automatically. If false, thunk must be provided as part of additionalMiddleware.
-***/
 export function createMiddleware(clientOptions) {
     const options = Object.assign({}, defaultOptions, clientOptions);
     const {
