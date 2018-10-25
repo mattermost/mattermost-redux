f779fa822e7b2148385145ddf432ec164d6ce123
Miguel Espinoza <espinozamiguel349@gmail.com>
2018-10-25 17:40:29 -0400

    Fix issue with filter in getUnreadChannels

--- src/selectors/entities/channels.js
+++ src/selectors/entities/channels.js
@@ -554,7 +554,7 @@ export const getUnreadChannels = createIdsSelector(
             return [];
         }
 
-        const allUnreadChannels = unreadIds.filter((id) => channels[id] && channels[id].delete_at !== 0).map((id) => {
+        const allUnreadChannels = unreadIds.filter((id) => channels[id] && channels[id].delete_at === 0).map((id) => {
             const c = channels[id];
 
             if (c.type === General.DM_CHANNEL || c.type === General.GM_CHANNEL) {
