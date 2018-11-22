138c99e9c76bcec053d7d6983ec8bddbb33dca16
Elias Nahum <nahumhbl@gmail.com>
2018-11-22 09:02:43 -0300

    Fix more DM's modal (#714)

--- src/selectors/entities/channels.js
+++ src/selectors/entities/channels.js
@@ -885,7 +885,7 @@ export const getChannelsWithUserProfiles = createSelector(
     getCurrentUserId,
     (channelUserMap, users, channels, currentUserId) => {
         return channels.map((channel) => {
-            const profiles = getProfiles(currentUserId, channelUserMap[channel.id], users);
+            const profiles = getProfiles(currentUserId, channelUserMap[channel.id] || [], users);
             return Object.assign({}, channel, {profiles});
         });
     }
