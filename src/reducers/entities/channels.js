8742a217f6b012375f46a055e37e52751dbf417b
Jesús Espino <jespinog@gmail.com>
2019-10-22 19:17:49 +0200

    Adding unit tests

--- src/reducers/entities/channels.js
+++ src/reducers/entities/channels.js
@@ -311,7 +311,7 @@ function myMembers(state = {}, action) {
     }
 }
 
-function membersInChannel(state = {}, action) {
+export function membersInChannel(state = {}, action) {
     switch (action.type) {
     case ChannelTypes.RECEIVED_MY_CHANNEL_MEMBER:
     case ChannelTypes.RECEIVED_CHANNEL_MEMBER: {
@@ -352,7 +352,7 @@ function membersInChannel(state = {}, action) {
         if (action.data) {
             const data = action.data;
             const members = {...(state[data.id] || {})};
-            if (members) {
+            if (state[data.id]) {
                 Reflect.deleteProperty(members, data.user_id);
                 return {
                     ...state,
