c3f234c181d5166581dbbb0e85756740588833ee
lebedev.vlaimir <lebedev.vladimir3@gmail.com>
2019-12-08 18:39:42 +0300

    MM-18903 - Removal of unused status flags in Integrations requests state objec

--- src/reducers/requests/index.ts
+++ src/reducers/requests/index.ts
@@ -10,7 +10,6 @@ import posts from './posts';
 import teams from './teams';
 import users from './users';
 import preferences from './preferences';
-import integrations from './integrations';
 import admin from './admin';
 import jobs from './jobs';
 import search from './search';
@@ -26,7 +25,6 @@ export default combineReducers({
     teams,
     users,
     preferences,
-    integrations,
     admin,
     jobs,
     search,
