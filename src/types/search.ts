87679d0dbf445f2552f2f3fbe3bdb8b543a56fc7
Hossein Ahmadian-Yazdi <hyazdi1997@gmail.com>
2020-03-30 13:20:55 -0400

    fix import path

--- src/types/search.ts
+++ src/types/search.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {Dictionary} from 'types/utilities';
+import {Dictionary} from './utilities';
 
 export type Search = {
     terms: string;
