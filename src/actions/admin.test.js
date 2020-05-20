602d33abbf63a20dcf176dcf360ba90241a7d756
Catalin Tomai <catalin.tomai@mattermost.com>
2020-05-20 13:58:52 -0700

    fix test

--- src/actions/admin.test.js
+++ src/actions/admin.test.js
@@ -1212,12 +1212,12 @@ describe('Actions.Admin', () => {
 
     it('sendWarnMetricAck', async () => {
         const warnMetricAck = {
-            metric1: true,
+            id: 'metric1',
         };
         nock(Client4.getBaseRoute()).
             post('/warn_metrics/ack').
             reply(200, OK_RESPONSE);
 
-        await Actions.sendWarnMetricAck(warnMetricAck)(store.dispatch, store.getState);
+        await Actions.sendWarnMetricAck(warnMetricAck.id, false)(store.dispatch, store.getState);
     });
 });
