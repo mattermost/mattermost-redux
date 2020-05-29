93152e01809b06c0b7a588a28acb9a3f73b71f77
Martin Kraft <martinkraft@gmail.com>
2020-05-29 10:47:23 -0400

    MM-25040: Omits some groups from channels where the team is group-con… (#1161)
    * MM-25040: Omits some groups from channels where the team is group-constrained.
    
    * MM-25040: Freezes the new test state.
    
    * MM-25040: Does a deepEqual inspection of the results.
    
    * MM-25040: Switches some Object.entries to Object.values.
    
    * MM-25040: Adds new action parameter/query parameter.

--- src/actions/groups.ts
+++ src/actions/groups.ts
@@ -196,7 +196,7 @@ export function getGroupsNotAssociatedToTeam(teamID: string, q = '', page = 0, p
     });
 }
 
-export function getGroupsNotAssociatedToChannel(channelID: string, q = '', page = 0, perPage: number = General.PAGE_SIZE_DEFAULT): ActionFunc {
+export function getGroupsNotAssociatedToChannel(channelID: string, q = '', page = 0, perPage: number = General.PAGE_SIZE_DEFAULT, filterParentTeamPermitted = false): ActionFunc {
     return bindClientFunc({
         clientFunc: Client4.getGroupsNotAssociatedToChannel,
         onSuccess: [GroupTypes.RECEIVED_GROUPS],
@@ -205,6 +205,7 @@ export function getGroupsNotAssociatedToChannel(channelID: string, q = '', page 
             q,
             page,
             perPage,
+            filterParentTeamPermitted,
         ],
     });
 }
--- src/selectors/entities/groups.test.js
+++ src/selectors/entities/groups.test.js
@@ -90,6 +90,9 @@ describe('Selectors.Groups', () => {
                 },
             },
             teams: {
+                teams: {
+                    [teamID]: {group_constrained: false},
+                },
                 groupsAssociatedToTeam: {
                     [teamID]: {ids: teamAssociatedGroupIDs},
                 },
@@ -133,8 +136,16 @@ describe('Selectors.Groups', () => {
     });
 
     it('getGroupsNotAssociatedToChannel', () => {
-        const expected = Object.entries(testState.entities.groups.groups).filter(([groupID]) => !channelAssociatedGroupIDs.includes(groupID)).map(([, group]) => group);
-        assert.deepEqual(Selectors.getGroupsNotAssociatedToChannel(testState, channelID), expected);
+        let expected = Object.values(testState.entities.groups.groups).filter((group) => !channelAssociatedGroupIDs.includes(group.id));
+        assert.deepEqual(Selectors.getGroupsNotAssociatedToChannel(testState, channelID, teamID), expected);
+
+        let cloneState = JSON.parse(JSON.stringify(testState));
+        cloneState.entities.teams.teams[teamID].group_constrained = true;
+        cloneState.entities.teams.groupsAssociatedToTeam[teamID].ids = [expectedAssociatedGroupID1];
+        cloneState = deepFreezeAndThrowOnMutation(cloneState);
+
+        expected = Object.values(cloneState.entities.groups.groups).filter((group) => !channelAssociatedGroupIDs.includes(group.id) && cloneState.entities.teams.groupsAssociatedToTeam[teamID].ids.includes(group.id));
+        assert.deepEqual(Selectors.getGroupsNotAssociatedToChannel(cloneState, channelID, teamID), expected);
     });
 
     it('getGroupsAssociatedToTeamForReference', () => {
