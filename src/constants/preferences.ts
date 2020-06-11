880b05a366ef9b0abfb9844409abf3ee6f84e1b4
Devin Binnie <devin.binnie@mattermost.com>
2020-06-11 15:15:31 -0400

    Typo

--- src/constants/preferences.ts
+++ src/constants/preferences.ts
@@ -42,7 +42,7 @@ const Preferences: Dictionary<any> = {
     ADVANCED_CODE_BLOCK_ON_CTRL_ENTER: 'code_block_ctrl_enter',
     ADVANCED_SEND_ON_CTRL_ENTER: 'send_on_ctrl_enter',
     CATEGORY_WHATS_NEW_MODAL: 'whats_new_modal',
-    HAS_SEEN_SIDEBAR_WHATS_NEW_MODAL: 'has_seen_whats_new_modal',
+    HAS_SEEN_SIDEBAR_WHATS_NEW_MODAL: 'has_seen_sidebar_whats_new_modal',
     CATEGORY_THEME: 'theme',
     THEMES: {
         default: {
