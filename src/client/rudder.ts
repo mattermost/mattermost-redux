9e508bc6abdbaac582ba7a1d6508d622c4271705
Sudheer <sudheer.105@gmail.com>
2020-06-16 23:21:43 +0530

    Update src/client/rudder.ts
    Co-authored-by: Jesse Hallam <jesse.hallam@gmail.com>

--- src/client/rudder.ts
+++ src/client/rudder.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-// the module exports a bunch of api's on a already defined object combined with node module caching,
-// selectively exporting which can be used throughout the project
+// As per rudder-sdk-js documentation, import this only once and use like a singleton.
+// See https://github.com/rudderlabs/rudder-sdk-js#step-1-install-rudderstack-using-the-code-snippet
 import * as rudderAnalytics from 'rudder-sdk-js';
 export {rudderAnalytics};
