d465f41cf007b05e95b7d2a83d1234561a87afe4
Harrison Healey <harrisonmhealey@gmail.com>
2020-08-14 13:13:25 -0400

    Remove WebSocket actions (#1215)
    * Remove WebSocket actions
    
    * Move WebSocket types into their own file for future use
    
    * Remove unused imports

---
+++ src/types/websocket.ts
@@ -0,0 +1,16 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+export type WebSocketBroadcast = {
+    omit_users: Record<string, boolean>;
+    user_id: string;
+    channel_id: string;
+    team_id: string;
+}
+
+export type WebSocketMessage<T> = {
+    event: string;
+    data: T;
+    broadcast: WebSocketBroadcast;
+    seq: number;
+}
