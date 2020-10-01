d208d32461f9ae4f1037d6c29410242a0f53a943
Nick Misasi <nick.misasi@mattermost.com>
2020-10-01 11:50:00 -0400

    Change dollar_per_seat to price_per_seat (#1251)

--- src/types/cloud.ts
+++ src/types/cloud.ts
@@ -5,7 +5,7 @@ export type Product = {
     id: string;
     name: string;
     description: string;
-    dollars_per_seat: number;
+    price_per_seat: number;
     add_ons: AddOn[];
 };
 
@@ -13,7 +13,7 @@ export type AddOn = {
     id: string;
     name: string;
     display_name: string;
-    dollars_per_seat: number;
+    price_per_seat: number;
 };
 
 export type CloudState = {
