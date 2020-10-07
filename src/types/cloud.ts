9479b8a953c07490c740e8859579da8438d1c52a
Nick Misasi <nick.misasi@mattermost.com>
2020-10-07 09:14:57 -0400

    Fix indentation

--- src/types/cloud.ts
+++ src/types/cloud.ts
@@ -17,8 +17,8 @@ export type Subscription = {
 	start_at: number;
 	end_at: number;
 	create_at: number;
-    seats: number;
-    is_paid_tier: string;
+	seats: number;
+	is_paid_tier: string;
 }
 
 export type Product = {
