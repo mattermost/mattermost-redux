0e563579577f5d8fd6acaad9ad6e80681a94bd9a
Devin Binnie <52460000+devinbinnie@users.noreply.github.com>
2020-10-19 11:57:16 -0400

    [MM-28218] Redux hooks for updating company info and address (#1259)
    * [MM-28218] Redux hooks for updating company info and address
    
    * Update type to make changed customer parameters optional

--- src/actions/cloud.ts
+++ src/actions/cloud.ts
@@ -4,6 +4,7 @@ import {CloudTypes} from 'action_types';
 import {Client4} from 'client';
 
 import {ActionFunc} from 'types/actions';
+import {Address, CloudCustomerPatch} from 'types/cloud';
 
 import {bindClientFunc} from './helpers';
 
@@ -25,5 +26,21 @@ export function getCloudCustomer(): ActionFunc {
     return bindClientFunc({
         clientFunc: Client4.getCloudCustomer,
         onSuccess: [CloudTypes.RECEIVED_CLOUD_CUSTOMER],
+    });
+}
+
+export function updateCloudCustomer(customerPatch: CloudCustomerPatch): ActionFunc {
+    return bindClientFunc({
+        clientFunc: Client4.updateCloudCustomer,
+        onSuccess: [CloudTypes.RECEIVED_CLOUD_CUSTOMER],
+        params: [customerPatch],
+    });
+}
+
+export function updateCloudCustomerAddress(address: Address): ActionFunc {
+    return bindClientFunc({
+        clientFunc: Client4.updateCloudCustomerAddress,
+        onSuccess: [CloudTypes.RECEIVED_CLOUD_CUSTOMER],
+        params: [address],
     });
 }
