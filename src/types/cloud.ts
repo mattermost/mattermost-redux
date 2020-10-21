24c5ca06b4da9db15fe3a6b6a0d7de1a8f45ff52
Devin Binnie <52460000+devinbinnie@users.noreply.github.com>
2020-10-21 09:21:26 -0400

    [MM-28214][MM-28216] Redux hooks for getting invoices (#1266)
    * [MM-28214][MM-28216] Redux hooks for getting invoices
    
    * Remove selector and add field for last invoice on Subscription
    
    * Make last_invoice optional

--- src/types/cloud.ts
+++ src/types/cloud.ts
@@ -7,6 +7,7 @@ export type CloudState = {
     subscription?: Subscription;
 	products?: Dictionary<Product>;
 	customer?: CloudCustomer;
+	invoices?: Dictionary<Invoice>;
 }
 
 export type Subscription = {
@@ -19,6 +20,7 @@ export type Subscription = {
 	create_at: number;
 	seats: number;
 	is_paid_tier: string;
+	last_invoice?: Invoice;
 }
 
 export type Product = {
@@ -78,4 +80,27 @@ export type PaymentMethod = {
 	exp_year: number;
 	card_brand: string;
 	name: string;
+}
+
+// Invoice model represents a invoice on the system.
+export type Invoice = {
+	id: string;
+	number: string;
+	create_at: number;
+	total: number;
+	status: string;
+	description: string;
+	period_start: number;
+	period_end: number;
+	subscription_id: string;
+	line_items: InvoiceLineItem[];
+}
+
+// InvoiceLineItem model represents a invoice lineitem tied to an invoice.
+export type InvoiceLineItem = {
+	price_id: string;
+	total: number;
+	quantity: number;
+	description: string;
+	metadata: Dictionary<string>;
 }
