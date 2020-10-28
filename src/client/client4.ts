0c473e7fdaf46e84d86a678b9d902c07ee1068c4
Mattermost Build <build@mattermost.com>
2020-10-28 16:28:15 +0100

    MM-29998 Add telemetry for in product notices (#1277) (#1278)
    * Add telemetry event notice_seen_NOTICEID
    
    (cherry picked from commit fdf8fa87579f6457d55f8999301fe52068ac67d6)
    
    Co-authored-by: Sudheer <sudheer.105@gmail.com>

--- src/client/client4.ts
+++ src/client/client4.ts
@@ -3349,6 +3349,8 @@ export default class Client4 {
     };
 
     updateNoticesAsViewed = (noticeIds: string[]) => {
+        // Only one notice is marked as viewed at a time so using 0 index
+        this.trackEvent('ui', `notice_seen_${noticeIds[0]}`);
         return this.doFetch<StatusOK>(
             `${this.getNoticesRoute()}/view`,
             {method: 'put', body: JSON.stringify(noticeIds)},
