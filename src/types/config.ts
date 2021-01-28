9bf77eb6baeb7868d842e726aa04ef9bcf9b0d4b
Caleb Roseland <caleb@calebroseland.com>
2021-01-27 22:53:00 -0600

    defer always_on config option

--- src/types/config.ts
+++ src/types/config.ts
@@ -23,7 +23,7 @@ export type ClientConfig = {
     BuildHashEnterprise: string;
     BuildNumber: string;
     CloseUnusedDirectMessages: string;
-    CollapsedThreads: 'disabled' | 'default_off' | 'default_on' | 'always_on';
+    CollapsedThreads: 'disabled' | 'default_off' | 'default_on';
     CustomBrandText: string;
     CustomDescriptionText: string;
     CustomTermsOfServiceId: string;
@@ -325,7 +325,7 @@ export type ServiceSettings = {
     EnableLocalMode: boolean;
     LocalModeSocketLocation: string;
     EnableLegacySidebar: boolean;
-    CollapsedThreads: 'disabled' | 'default_on' | 'default_off' | string;
+    CollapsedThreads: 'disabled' | 'default_on' | 'default_off';
 };
 
 export type TeamSettings = {
