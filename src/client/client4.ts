4142a2a0a19f414e2d2cd8d4d3da7a206cd38efe
Ben Schumacher <ben.schumacher@mattermost.com>
2021-02-05 17:32:41 +0100

    Fix signature of executeCommand (#1354)

--- src/client/client4.ts
+++ src/client/client4.ts
@@ -46,6 +46,7 @@ import {
 import {PostActionResponse} from 'types/integration_actions';
 import {
     Command,
+    CommandArgs,
     CommandResponse,
     DialogSubmission,
     IncomingWebhook,
@@ -2397,7 +2398,7 @@ export default class Client4 {
         );
     };
 
-    executeCommand = (command: Command, commandArgs = {}) => {
+    executeCommand = (command: string, commandArgs: CommandArgs) => {
         this.trackEvent('api', 'api_integrations_used');
 
         return this.doFetch<CommandResponse>(
