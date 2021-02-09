d15c5d68612f326594d48f4a63229a33c798bafb
Eli Yukelzon <reflog@gmail.com>
2021-02-09 12:03:52 +0200

    MM-32525 added unread_mentions_per_channel (#1353)

--- src/actions/threads.ts
+++ src/actions/threads.ts
@@ -50,6 +50,30 @@ export function getThreads(userId: string, teamId: string, {before = '', after =
     };
 }
 
+export function getThreadMentionCountsByChannel(teamId: string) {
+    return async (dispatch: DispatchFunc, getState: GetStateFunc) => {
+        let result: Record<string, number>;
+
+        try {
+            const {currentUserId} = getState().entities.users;
+            result = await Client4.getThreadMentionCountsByChannel(currentUserId, teamId);
+        } catch (error) {
+            forceLogoutIfNecessary(error, dispatch, getState);
+            dispatch(logError(error));
+            return {error};
+        }
+
+        dispatch({
+            type: ThreadTypes.RECEIVED_PER_CHANNEL_MENTION_COUNTS,
+            data: {
+                counts: result,
+                team_id: teamId,
+            },
+        });
+
+        return {data: result};
+    };
+}
 export function getThread(userId: string, teamId: string, threadId: string, extended = false) {
     return async (dispatch: DispatchFunc, getState: GetStateFunc) => {
         let thread;
