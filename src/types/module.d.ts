63ba0a8ef3f9122df31716fc733f7e3dc10b5838
Jesse Hallam <jesse.hallam@gmail.com>
2021-03-04 12:16:37 -0400

    remove remote-redux-devtools

--- src/types/module.d.ts
+++ src/types/module.d.ts
@@ -3,6 +3,5 @@
 declare module 'gfycat-sdk';
 declare module 'redux-offline';
 declare module 'redux-offline/lib/defaults';
-declare module 'remote-redux-devtools';
 declare module 'redux-persist';
 declare module 'redux-persist/constants';
