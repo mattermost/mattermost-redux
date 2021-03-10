3451926da0a45286f7c57c95edef5df3b08cdb33
Hossein Ahmadian-Yazdi <hyazdi1997@gmail.com>
2021-03-10 13:12:15 -0500

    initial commit

--- src/constants/permissions.ts
+++ src/constants/permissions.ts
@@ -83,8 +83,14 @@ const values = {
 
     SYSCONSOLE_READ_ABOUT: 'sysconsole_read_about',
     SYSCONSOLE_WRITE_ABOUT: 'sysconsole_write_about',
-    SYSCONSOLE_READ_BILLING: 'sysconsole_read_billing',
-    SYSCONSOLE_WRITE_BILLING: 'sysconsole_write_billing',
+    SYSCONSOLE_READ_BILLING_SUBSCRIPTION: 'sysconsole_read_billing_subscription',
+    SYSCONSOLE_WRITE_BILLING_SUBSCRIPTION: 'sysconsole_write_billing_subscription',
+    SYSCONSOLE_READ_BILLING_BILLING_HISTORY: 'sysconsole_read_billing_billing_history',
+    SYSCONSOLE_WRITE_BILLING_BILLING_HISTORY: 'sysconsole_write_billing_billing_history',
+    SYSCONSOLE_READ_BILLING_COMPANY_INFORMATION: 'sysconsole_read_billing_company_information',
+    SYSCONSOLE_WRITE_BILLING_COMPANY_INFORMATION: 'sysconsole_write_billing_company_information',
+    SYSCONSOLE_READ_BILLING_PAYMENT_INFORMATION: 'sysconsole_read_billing_payment_information',
+    SYSCONSOLE_WRITE_BILLING_PAYMENT_INFORMATION: 'sysconsole_write_billing_payment_information',
     SYSCONSOLE_READ_REPORTING: 'sysconsole_read_reporting',
     SYSCONSOLE_WRITE_REPORTING: 'sysconsole_write_reporting',
     SYSCONSOLE_READ_USERMANAGEMENT_USERS: 'sysconsole_read_user_management_users',
@@ -140,7 +146,10 @@ const values = {
 
 values.SYSCONSOLE_READ_PERMISSIONS = [
     values.SYSCONSOLE_READ_ABOUT,
-    values.SYSCONSOLE_READ_BILLING,
+    values.SYSCONSOLE_READ_BILLING_SUBSCRIPTION,
+    values.SYSCONSOLE_READ_BILLING_BILLING_HISTORY,
+    values.SYSCONSOLE_READ_BILLING_COMPANY_INFORMATION,
+    values.SYSCONSOLE_READ_BILLING_PAYMENT_INFORMATION,
     values.SYSCONSOLE_READ_REPORTING,
     values.SYSCONSOLE_READ_USERMANAGEMENT_USERS,
     values.SYSCONSOLE_READ_USERMANAGEMENT_GROUPS,
@@ -163,7 +172,10 @@ values.SYSCONSOLE_READ_PERMISSIONS = [
 
 values.SYSCONSOLE_WRITE_PERMISSIONS = [
     values.SYSCONSOLE_WRITE_ABOUT,
-    values.SYSCONSOLE_WRITE_BILLING,
+    values.SYSCONSOLE_WRITE_BILLING_SUBSCRIPTION,
+    values.SYSCONSOLE_WRITE_BILLING_BILLING_HISTORY,
+    values.SYSCONSOLE_WRITE_BILLING_COMPANY_INFORMATION,
+    values.SYSCONSOLE_WRITE_BILLING_PAYMENT_INFORMATION,
     values.SYSCONSOLE_WRITE_REPORTING,
     values.SYSCONSOLE_WRITE_USERMANAGEMENT_USERS,
     values.SYSCONSOLE_WRITE_USERMANAGEMENT_GROUPS,
