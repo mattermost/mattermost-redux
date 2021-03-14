d2d14e1d9a54f0de0c1deca15e503dc8682d81b5
Eric Montague <eric.g.montague@gmail.com>
2021-03-14 19:52:56 -0400

    Fixed styling issue for Dictionary import

--- src/types/config.ts
+++ src/types/config.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import { Dictionary } from './utilities';
+import {Dictionary} from './utilities';
 
 export type ClientConfig = {
     AboutLink: string;
